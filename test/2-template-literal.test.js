0ae444c56d496f58da1cf21fd5289b5f263fd33a
Iamck <thanhson_it201@yahoo.com>
2019-05-06 14:54:23 +0700

    first commit

--- test/2-template-literal.test.js
+++ test/2-template-literal.test.js
@@ -1,45 +1,50 @@
 test('should support string interpolation', () => {
     const person = {
-      name: 'Kent C. Dodds',
-      friends: [
-        'Brooke Dodds',
-        'Matt Zabriskie',
-        'Aaron Frost',
-        'Dave Geddes',
-        'Joe Eames',
-        'Ryan Florence',
-      ],
+        name: 'Kent C. Dodds',
+        friends: [
+            'Brooke Dodds',
+            'Matt Zabriskie',
+            'Aaron Frost',
+            'Dave Geddes',
+            'Joe Eames',
+            'Ryan Florence',
+        ],
     }
     // construct a string using template literal string interpolation
-    const personsFriends = ``
+    const personsFriends = `${person.name} has ${person.friends.length} friends: ${person.friends.join(', ')}`
     expect(personsFriends).toBe(
-      'Kent C. Dodds has 6 friends: Brooke Dodds, Matt Zabriskie, Aaron Frost, Dave Geddes, Joe Eames, Ryan Florence',
+        'Kent C. Dodds has 6 friends: Brooke Dodds, Matt Zabriskie, Aaron Frost, Dave Geddes, Joe Eames, Ryan Florence',
     )
-  })
-  
-  test(`should support multi-line strings`, () => {
+})
+
+test(`should support multi-line strings`, () => {
     // construct a string with multiple lines without needing escaped newline characters
-    const multiLine = ``
+    const multiLine = `
+    How cool
+    is this!?
+  `
+
     expect(multiLine).toBe('\n    How cool\n    is this!?\n  ')
-  })
-  
-  test(`should support string escaping`, () => {
+})
+
+test(`should support string escaping`, () => {
     // properly escape a string in a template literal for each of these
-    expect(``).toBe('Hi\nthere!')
-    expect(``).toBe('This is `escaped` backticks')
-  })
-  
-  // you likely won't often use tagging, but it can be handy!
-  test.skip(`should call the tagging function`, () => {
+    expect(`Hi
+there!`).toBe('Hi\nthere!')
+    expect(`This is \`escaped\` backticks`).toBe('This is `escaped` backticks')
+})
+
+// you likely won't often use tagging, but it can be handy!
+test(`should call the tagging function`, () => {
     const noun = 'World'
     const emotion = 'happy'
     const result = tagIt`Hello ${noun}! Are you feeling ${emotion} today?`
     expect(result).toBe(
-      'Hello super-cool World! Are you feeling really happy today?',
+        'Hello super-cool World! Are you feeling really happy today?',
     )
-  
+
     function tagIt(literalString, ...interpolatedParts) {
-      // implement this function to make the test pass
-      return 'fixme'
+        // implement this function to make the test pass
+        return `${literalString[0]}super-cool ${interpolatedParts[0]}${literalString[1]}really ${interpolatedParts[1]}${literalString[2]}`
     }
-  })
+})
